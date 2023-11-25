1113b9e2991ff1ddd44435440aa14db25b6c7369
rteferi <rteferi@lion.lmu.edu>
2023-11-24 19:52:35 -0800

    update to last commit04 realized i need to connect my webserver and jenkins

--- woocommerce_scripts/create_dummy_products.py
+++ woocommerce_scripts/create_dummy_products.py
@@ -30,7 +30,7 @@ except KeyError:
     raise Exception(f"The environment variables 'WOO_KEY' and 'WOO_SECRET' must be set. ")
 
 wcapi = API(
-    url="http://dev.bootcamp.store.supersqa.com/", #"http://localhost:8888/ecomtester",
+    url="http://localhost:8888/ecomtester", #"http://dev.bootcamp.store.supersqa.com/",
     consumer_key=woo_key,
     consumer_secret=woo_secret,
     version="wc/v3"
